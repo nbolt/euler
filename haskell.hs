1fff4cd3e584a2347144dc61b55309318a0f5ced
Chris Bolton <c@chrisbolton.me>
2012-11-04 13:18:13 -0800

    improved output handling

--- haskell.hs
+++ haskell.hs
@@ -1,9 +1,23 @@
-import Data.List(findIndex)
-import System(getArgs)
+import Data.List(find, findIndex)
+import System.Environment(getArgs)
 
 main = do
   args <- getArgs
   mapM_ solve args
+
+-- helper functions
+solved = [("1", one), ("2", two)]
+
+solve num =
+  let pair = find (\pair -> fst pair == num) solved in
+    case pair of
+      Nothing   -> putStrLn $ zeroPad num ++ ": unsolved"
+      Just pair -> putStrLn $ zeroPad num ++ ": " ++ show (snd pair)
+
+zeroPad str =
+  case length str of
+    1 -> "0" ++ str
+    _ -> str
 
 -- one
 one = sum [x | x <- [1..999], mod x 3 == 0 || mod x 5 == 0]
@@ -13,10 +27,3 @@ two = sum [x | x <- take limit fibs, even x]
 
 fibs = 0 : scanl (+) 1 fibs
 (Just limit) = findIndex (> 4000000) fibs
-
--- helper function
-solve x =
-  case x of
-    "1" -> putStrLn $ "01: " ++ show one
-    "2" -> putStrLn $ "02: " ++ show two
-    x   -> putStrLn $ "0" ++ x ++ ": not found/solved"
